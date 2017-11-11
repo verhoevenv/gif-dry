9b435cd67c7323f0ad959ec75248a4bbccbdf747
Vincent Verhoeven <verhoevenv@gmail.com>
2017-11-11 14:57:39 +0100

    Attempt to work around slow travis build

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 rm -rf target
 mkdir target
-elm-make src/Main.elm --output=target/main.js
+if [ "${TRAVIS}" == "true" ]; then
+    $TRAVIS_BUILD_DIR/sysconfcpus/bin/sysconfcpus -n 2 elm-make src/Main.elm --output=target/main.js
+else
+    elm-make src/Main.elm --output=target/main.js
+fi
 cp src/index.html target
