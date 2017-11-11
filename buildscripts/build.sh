886e1fbe7d6ed1074fa7eec9cddd310c9657151a
Vincent Verhoeven <verhoevenv@gmail.com>
2017-11-11 15:01:27 +0100

    Fix incorrect script

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf target
 mkdir target
-if [ "${TRAVIS}" == "true" ]; then
+if [ "${TRAVIS}" = "true" ]; then
     $TRAVIS_BUILD_DIR/sysconfcpus/bin/sysconfcpus -n 2 elm-make src/Main.elm --output=target/main.js
 else
     elm-make src/Main.elm --output=target/main.js
